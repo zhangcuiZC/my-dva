b539522f54e66c0ecb63d8562fe73b926b1dfd67
ZhangCui <zczhangcui@foxmail.com>
2017-03-31 15:09:58 +0800

    update

--- app4/src/models/users.js
+++ app4/src/models/users.js
@@ -1,3 +1,11 @@
+import { hashHistory } from 'dva/router';
+
+import request from '../utils/request';
+import qs from 'qs';
+async function query(params) {
+	return request(`/api/users?{qs.stringify(params)}`);
+}
+
 export default {
 	namespace: 'users',
 	state: {
@@ -11,40 +19,33 @@ export default {
 	},
 
 	effects: {
-		*query() {},
+		*query({ payload }, { select, call, put }) {
+			yield put({ type: 'showLoading' });
+			const { data } = yield call(query);
+			if (data) {
+				yield put({
+					type: 'querySuccess',
+					payload: {
+						list: data.data,
+						total: data.page.total,
+						current: data.page.current
+					}
+				});
+			}
+		},
 		*create() {},
 		*'delete'() {},
 		*update() {},
 	},
 
 	reducers: {
-		showLoading() {},
+		showLoading(state, action) {
+			return { ...state, loading: true };
+		},
 		showModal() {},
 		hideModal() {},
-		querySuccess(state) {
-			const mock = {
-				total: 3,
-				current: 1,
-				loading: false,
-				list: [
-					{
-						name: '张三',
-						age: 23,
-						address: '成都',
-					},
-					{
-						name: '李四',
-						age: 24,
-						address: '杭州',
-					},
-					{
-						name: '王五',
-						age: 25,
-						address: '上海',
-					},
-				],
-			};
-			return { ...state, ...mock, loading: false };
+		querySuccess(state, action) {
+			return { ...state, ...action.payload, loading: false };
 		},
 		createSuccess() {},
 		deleteSuccess() {},
@@ -56,7 +57,7 @@ export default {
 			history.listen(location => {
 				if (location.pathname === '/users') {
 					dispatch({
-						type: 'querySuccess',
+						type: 'query',
 						payload: {}
 					});
 				}
